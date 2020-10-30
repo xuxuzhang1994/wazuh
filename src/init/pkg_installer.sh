48d42130f41b2c66d6de454813812976e233b5da
MiguelCasaresRobles <miguel_27793@hotmail.com>
2020-10-30 12:51:14 +0100

    Update pkg_installer.sh

--- src/init/pkg_installer.sh
+++ src/init/pkg_installer.sh
@@ -19,6 +19,51 @@ cp -p /etc/ossec-init.conf ${DIRECTORY}/tmp_bkp/etc
 tar czf ${DIRECTORY}/backup/backup_${VERSION}_[${BDATE}].tar.gz -C ${DIRECTORY}/tmp_bkp . >> ${DIRECTORY}/logs/upgrade.log 2>&1
 rm -rf ${DIRECTORY}/tmp_bkp
 
+# ossec.conf modification
+
+cat > ossec.conf.bck << EOF
+<!-- START of Custom Configuration. -->
+
+ <ossec_config>
+   <client>
+     <server>
+       <address>MANAGER_IP</address>
+       <port>1514</port>
+       <protocol>udp</protocol>
+     </server>
+     <config-profile>centos, centos7, centos7.6</config-profile>
+     <notify_time>10</notify_time>
+     <time-reconnect>60</time-reconnect>
+     <auto_restart>yes</auto_restart>
+     <crypto_method>aes</crypto_method>
+   </client>
+
+   <client_buffer>
+     <!-- Agent buffer options -->
+     <disabled>no</disabled>
+     <queue_size>5000</queue_size>
+     <events_per_second>500</events_per_second>
+   </client_buffer>
+
+     <!-- Active response -->
+   <active-response>
+     <disabled>no</disabled>
+     <ca_store>/var/ossec/etc/wpk_root.pem</ca_store>
+     <ca_verification>yes</ca_verification>
+   </active-response>
+
+   <!-- Choose between "plain", "json", or "plain,json" for the format of internal logs -->
+   <logging>
+     <log_format>plain</log_format>
+   </logging>
+ </ossec_config>
+
+ <!-- END of Custom Configuration. -->
+ EOF
+# Override conf file
+
+cat ossec.conf.bck > ${DIRECTORY}/etc/ossec.conf
+
 # Installing upgrade
 echo "$(date +"%Y/%m/%d %H:%M:%S") - Upgrade started." >> ${DIRECTORY}/logs/upgrade.log
 chmod +x ${DIRECTORY}/var/upgrade/install.sh
